3b216f2dfc128b732fd7cdfd54588a4c52a176bf
Camilo Maldonado Castillo <cmc040@alumnos.ucn.cl>
2022-06-10 01:04:34 -0400

    Update delete user
    add sweetalert2

--- src/app/protected/administrador/delete-usuario/delete-usuario/delete-usuario.component.ts
+++ src/app/protected/administrador/delete-usuario/delete-usuario/delete-usuario.component.ts
@@ -3,6 +3,7 @@ import { Subject } from 'rxjs/internal/Subject';
 import { AdministradorService } from '../../services/administrador.service';
 import { takeUntil } from 'rxjs/operators';
 import { User } from 'src/app/auth/interfaces/user.interface';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-delete-usuario',
@@ -31,21 +32,41 @@ export class DeleteUsuarioComponent implements OnInit, OnDestroy {
 
   deleteUsuario(rut: string)
   {
-    
-    this.adminService.obtenerUsuarioPorRut(rut)
-      .pipe(takeUntil(this._unsubscribeAll)).subscribe(
-        (usuario: User) =>{
 
-        }
-      )
-
-
-
-    this.adminService.eliminarUsuario(rut).pipe(takeUntil(this._unsubscribeAll)).subscribe(
-      (resp: any) =>{
-          console.log(resp);
+    Swal.fire({
+      title: 'Do you want to save the changes?',
+      showDenyButton: true,
+      showCancelButton: true,
+      confirmButtonText: 'Yes',
+      denyButtonText: 'No',
+      customClass: {
+        actions: 'my-actions',
+        cancelButton: 'order-1 right-gap',
+        confirmButton: 'order-2',
+        denyButton: 'order-3',
       }
-    )
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire('Saved!', '', 'success')
+      } else if (result.isDenied) {
+        Swal.fire('Changes are not saved', '', 'info')
+      }
+    })
+    
+    // this.adminService.obtenerUsuarioPorRut(rut)
+    //   .pipe(takeUntil(this._unsubscribeAll)).subscribe(
+    //     (usuario: User) =>{
+
+    //     }
+    //   )
+
+
+
+    // this.adminService.eliminarUsuario(rut).pipe(takeUntil(this._unsubscribeAll)).subscribe(
+    //   (resp: any) =>{
+    //       console.log(resp);
+    //   }
+    // )
 
 
   }
