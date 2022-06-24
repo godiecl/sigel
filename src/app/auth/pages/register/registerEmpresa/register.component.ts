c032d36119ee98a4b17193c6ca0f7eacf8999460
Walter Sierra <walter.sierra@alumnos.ucn.cl>
2022-06-24 18:18:55 -0400

    update register contacto y empresa

--- src/app/auth/pages/register/registerEmpresa/register.component.ts
+++ src/app/auth/pages/register/registerEmpresa/register.component.ts
@@ -8,6 +8,11 @@ import Swal from 'sweetalert2';
 import { Subject } from 'rxjs/internal/Subject';
 import { takeUntil } from 'rxjs/operators';
 import { RutService } from 'rut-chileno';
+import { EncargadoEmpresa } from '../../../interfaces/encargadoEmpresa.interface';
+import { EncargadoEmpresaModel } from '../../../models/encargadoEmpresa.model';
+import { User } from 'src/app/auth/interfaces/user.interface';
+import { UserModel } from 'src/app/auth/models/user.model';
+import { AdministradorService } from '../../../../protected/administrador/services/administrador.service';
 
 @Component({
   selector: 'app-register',
@@ -16,10 +21,13 @@ import { RutService } from 'rut-chileno';
   ]
 })
 export class RegisterComponent implements OnInit, OnDestroy {
-
+  
+  usuario!: User
   empresa!: Empresa;
   private _unsubscribeAll: Subject<any>;
   emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
+  encargadoEmpresa!: EncargadoEmpresa;
+  idEmpresaActual!: number;
 
   empresaForm: FormGroup = this.fb.group({
       rutEmpresa: ['', [Validators.required, this.rutService.validaRutForm]],
@@ -39,6 +47,7 @@ export class RegisterComponent implements OnInit, OnDestroy {
   constructor(private fb:FormBuilder,
               private router: Router,
               private authService: AuthService,
+              private adminService: AdministradorService,
               private rutService: RutService
               ) { 
                 this._unsubscribeAll = new Subject();
@@ -84,55 +93,110 @@ export class RegisterComponent implements OnInit, OnDestroy {
         denyButton: 'order-3',
       }
     }).then((result) => {
+      // SI OPRIME BOTON SI.
       if (result.isConfirmed) {
 
         // console.log('valor formulario de crear empresa:',this.empresaForm.value);
         // console.log('formulario de crear empresa, valido? ',this.empresaForm.valid);
 
+        // PRIMERO SE CREA LA EMPRESA
+
         this.empresa = new EmpresaModel(0,
           this.empresaForm.value.nombreEmpresa,this.empresaForm.value.rutEmpresa, this.empresaForm.value.giroEmpresa
         );
 
+        console.log('this empresa', this.empresa)
+        // SE REGISTRA LA EMPRESA
         this.authService.crearEmpresa(this.empresa).
           // pipe(takeUntil(this._unsubscribeAll)).
             subscribe( resp => {
-            // console.log('respuesta', resp);
-            try {
+            console.log('respuesta CREAR EMPRESA', resp);
+              
+            if(resp.ok){
+               
+              // SI SE CREÓ EXITOSAMENTE LA EMPRESA, SE GUARDA SU ID
+
+              Swal.fire('Se ha registrado su empresa exitosamente', '', 'success');
+              this.idEmpresaActual = resp.newEmpresa.id_empresa;
                // console.log('empresa id', empresa.id_empresa);
-               this.authService.actualizarEmpresaActual(resp.newEmpresa.id_empresa);
+              //  this.authService.actualizarEmpresaActual(resp.newEmpresa.id_empresa);
                // let idEmpresaxd;
                // this.authService.empresaActual.subscribe(idEmpresa => idEmpresaxd = idEmpresa )
                //  console.log('subject',idEmpresaxd )
-               Swal.fire('Se ha registrado su empresa exitosamente', '', 'success');
-               // this.router.navigateByUrl('/auth/solicitar-estudiante');
-              
-            } catch (error) {
-              Swal.fire('No se ha registrado', '', 'info')
-            }
-            if(resp.ok){
+               // this.router.navigateByUrl('/auth/solicitar-estudiante');        
               // this.authService.actualizarEmpresaActual(resp.newEmpresa.id_empresa);
               // Swal.fire('Se ha registrado su empresa exitosamente', '', 'success');
-
               // let idEmpresaxd;
               // this.authService.empresaActual.subscribe(idEmpresa => idEmpresaxd = idEmpresa )
               //  console.log('subject',idEmpresaxd )
               // this.router.navigateByUrl('/auth/solicitar-estudiante');
-            }
-            
-            
-          }
-          )
 
-       
+              
+              // SE CREA EL USUARIO
+              this.usuario = new UserModel( this.empresaForm.value );
+              this.usuario.password = this.empresaForm.value.password;
+              this.usuario.roles = [...this.usuario.roles, 'EncargadoEmpresa'];
+              this.usuario.estado = false;
+              console.log('this usuario', this.usuario)
+
+              // SE REGISTRA EN LA BDD
+              this.adminService.crearUsuario(this.usuario)
+                .subscribe((resp: any)=>{
+                  if(resp.ok){
+                    // se inicializa el encargado
+
+                    this.encargadoEmpresa = new EncargadoEmpresaModel(0,
+                    this.empresaForm.value.cargo, this.empresaForm.value.telefono, resp.id, this.idEmpresaActual)
+                    console.log('encargado empresa', this.encargadoEmpresa)
+                    // se manda a la bdd
+                    this.authService.crearEncargadoEmpresa(this.encargadoEmpresa)
+                    .subscribe((response: any)=>{
+                      // console.log(response);
+                      if(response.ok){
+
+                      Swal.fire('Se ha registrado su contacto en el sistema.', '', 'success');
+                      // this.authService.actualizarEncargadoEmpresaActual(response.id)
+                      // this.router.navigateByUrl('/auth/solicitar-estudiante');
+
+                      }
+                      else if(!response.ok){
+                        Swal.fire('Ha ocurrido un error al registrarlo. Intente nuevamente.','','error');
+                      }
+                      
+                    
+                      })
+                    }else{
+                    // imprimir error
+                    Swal.fire('Ha ocurrido un error.', '', 'info')
+                    // console.log('aqui fallo')
+                    }
+                  })
+                }else{
+                  Swal.fire('No se ha registrado', '', 'info')
+                }            
+          })
+
 
         //se borra todo lo que contiene el formulario
         //this.empresaForm.reset();
-      } else if (result.isDenied) {
+      } 
+      // SI OPRIME EL BOTON NO.
+      else if (result.isDenied) {
         Swal.fire('No se ha registrado', '', 'info')
       }
+
+        
+
+        
+
+        // this.encargadoEmpresa = new EncargadoEmpresaModel(
+        //   0, this.empresaForm.value.cargo, this.empresaForm.value.telefono, 
+        // )
+
+        
+
+        
+
     })
-
-    
   }
-
 }
