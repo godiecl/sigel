69cdc6c2d0479e6a54a846328b2b8e3ccaaa8673
Camilo Maldonado Castillo <cmc040@alumnos.ucn.cl>
2022-07-04 22:44:44 -0400

    Update comision-titulacion-practica.service.ts
    add last version

--- src/app/protected/comision-titulacion-practica/comision-titulacion-practica.service.ts
+++ src/app/protected/comision-titulacion-practica/comision-titulacion-practica.service.ts
@@ -1,7 +1,8 @@
+import { tap } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Publicacion } from '../../auth/interfaces/documentos/publicacion.interface';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, Observable, Subject  } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -10,6 +11,7 @@ import { environment } from 'src/environments/environment';
 export class ComisionTitulacionPracticaService {
 
   private baseUrl: string = environment.baseUrl;
+  private _refresh$ = new Subject<void>();
   // private _publicaciones: BehaviorSubject<any[]> = new BehaviorSubject([]);
 
   // getPublicaciones(): Observable<any[]>
@@ -24,7 +26,9 @@ export class ComisionTitulacionPracticaService {
   //   }
 
   constructor(private http: HttpClient) { }
-
+  get refresh$(){
+    return this._refresh$;
+  }
   getPublicaciones() :Observable<Publicacion[]> {
     const url = `${this.baseUrl}publicaciones`;
     return this.http.get<any>(url)
@@ -52,14 +56,130 @@ export class ComisionTitulacionPracticaService {
     return this.http.patch<any>(url, {publicacion});
   }
 
+  //contenido practica
   sendPostContenidoPractica(body:FormData): Observable <any>{
     const url= `${this.baseUrl}upload-contenido/practica`;
     return this.http.post(url,body);
-
   }
+  //contenido capstone
   sendPostContenidoCapstone(body:FormData): Observable <any>{
     const url= `${this.baseUrl}upload-contenido/capstone`;
     return this.http.post(url,body);
+  }
+  //documentos practica-estudiante
+  sendPostDocPracticaEstudiante(body:FormData): Observable <any>{
+    const url= `${this.baseUrl}upload-doc/practica-estudiante`;
+    return this.http.post(url,body).pipe(
+      tap(()=>{
+        this._refresh$.next();
+      })
+    );
+  }
+  downloadFileDocPracticaEstudiante(file:String){
+    var body = {filename:file};
+    const url=`${this.baseUrl}download-doc/practica-estudiante`;
+    return this.http.post(url,body,{responseType:'blob'});
+  }
+  getFilesDocPracticaEstudiante(): Observable<any> {
+    const url=`${this.baseUrl}doc/practica-estudiante`;
+    return this.http.get(url);
+  }
+  deleteFileDocPracticaEstudiante(file:String): Observable <any>{
+    var body = {filename:file};
+    const url=`${this.baseUrl}delete-doc/practica-estudiante`;
+    return this.http.post(url,body)
+    .pipe(
+      tap(()=>{
+        this._refresh$.next();
+      })
+    );
+  }
+  //documentos capstone-estudiante
+  sendPostDocCapstoneEstudiante(body:FormData): Observable <any>{
+    const url= `${this.baseUrl}upload-doc/capstone-estudiante`;
+    return this.http.post(url,body)
+    .pipe(
+      tap(()=>{
+        this._refresh$.next();
+      })
+    );
+  }
+  downloadFileDocCapstoneEstudiante(file:String){
+    var body = {filename:file};
+    const url=`${this.baseUrl}download-doc/capstone-estudiante`;
+    return this.http.post(url,body,{responseType:'blob'});
+  }
+  getFilesDocCapstoneEstudiante(): Observable<any> {
+    const url=`${this.baseUrl}doc/capstone-estudiante`;
+    return this.http.get(url);
+  }
+  deleteFileDocCapstoneEstudiante(file:String): Observable <any>{
+    var body = {filename:file};
+    const url=`${this.baseUrl}delete-doc/capstone-estudiante`;
+    return this.http.post(url,body)
+    .pipe(
+      tap(()=>{
+        this._refresh$.next();
+      })
+    );
+  }
+  //documentos practica-profesor
+  sendPostDocPracticaProfesor(body:FormData): Observable <any>{
+    const url= `${this.baseUrl}upload-doc/practica-profesor`;
+    return this.http.post(url,body)
+    .pipe(
+      tap(()=>{
+        this._refresh$.next();
+      })
+    );
+  }
+  downloadFileDocPracticaProfesor(file:String){
+    var body = {filename:file};
+    const url=`${this.baseUrl}download-doc/practica-profesor`;
+    return this.http.post(url,body,{responseType:'blob'});
+  }
+  getFilesDocPracticaProfesor(): Observable<any> {
+    const url=`${this.baseUrl}doc/practica-profesor`;
+    return this.http.get(url);
+  }
+  deleteFileDocPracticaProfesor(file:String){
+    var body = {filename:file};
+    const url=`${this.baseUrl}delete-doc/practica-profesor`;
+    return this.http.post(url,body)
+    .pipe(
+      tap(()=>{
+        this._refresh$.next();
+      })
+    );  
+  }
+
+  //documentos capstone profesor//
+  sendPostDocCapstoneProfesor(body:FormData): Observable <any>{
+    const url= `${this.baseUrl}upload-doc/capstone-profesor`;
+    return this.http.post(url,body)
+    .pipe(
+      tap(()=>{
+        this._refresh$.next();
+      })
+    );
+  }
+  downloadFileDocCapstoneProfesor(file:String){
+    var body = {filename:file};
+    const url=`${this.baseUrl}download-doc/capstone-profesor`;
+    return this.http.post(url,body,{responseType:'blob'});
+  }
+  getFilesDocCapstoneProfesor(): Observable<any> {
+    const url=`${this.baseUrl}doc/capstone-profesor`;
+    return this.http.get(url);
+  }
+  deleteFileDocCapstoneProfesor(file:String){
+    var body = {filename:file};
+    const url=`${this.baseUrl}delete-doc/capstone-profesor`;
+    return this.http.post(url,body)
+    .pipe(tap(()=>{
+        this._refresh$.next();
+      })
+    );
   }
 }
 
